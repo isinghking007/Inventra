a97480edccb518fecd1c112045a7bd7e060f7a02
isinghking007 <007ravinash@gmail.com>
2026-05-23 09:17:02 +0530

    UI - Registration Page

--- UI/InventraUI/src/app/app.routes.ts
+++ UI/InventraUI/src/app/app.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
-
+import { RegistrationComponent } from './UserManagement/Authentication/registration/registration.component';
+import { LoginComponent } from './UserManagement/Authentication/login/login.component';
+import {AuthComponent} from './UserManagement/Authentication/auth/auth.component'
 export const routes: Routes = [
-
+{
+    path:'auth',children:[
+        {
+            path:'register',component:RegistrationComponent
+        },
+        {
+            path:'login',component:LoginComponent
+        }
+    ],component:AuthComponent
+}
 ];
