56f7c5e6122626035a1e502c904eb816e4105bb4
isinghking007 <007ravinash@gmail.com>
2026-05-23 09:05:03 +0530

    UI - Registration Page

---
+++ Inventra/Inventra/Inventra.Domain/Entities/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventra.Domain.Entities
+{
+    public class Inventory
+    {
+        [Key]
+        public int InventoryID { get; set; }
+
+        [ForeignKey("ProductIdRef")]
+        public Product ProductId { get; set; }
+
+        public int TotalQuantity { get; set; }
+        public int ReservedQuantity { get; set; }
+        public int AvailableQuantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
